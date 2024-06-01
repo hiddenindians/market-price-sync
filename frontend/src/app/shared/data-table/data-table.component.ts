88ff10badd59103a5ce8c2c0e31ba21bbbeb9983
hiddenindians <amanthethy@gmail.com>
2024-06-01 11:56:00 -0500

    Datatable w/ pagination.

--- frontend/src/app/shared/data-table/data-table.component.ts
+++ frontend/src/app/shared/data-table/data-table.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-data-table',
@@ -15,18 +15,31 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 export class DataTableComponent implements OnChanges {
   @Input() data: any[] = [];
   @Input() displayedColumns: string[] = [];
+  @Input() totalLength: number = 0;
+  @Output() page: EventEmitter<PageEvent> =  new EventEmitter<PageEvent>();
   dataSource: MatTableDataSource<any>;
-
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-
   constructor() {
-    this.dataSource = new MatTableDataSource(); // Ensure dataSource is initialized
+    this.dataSource = new MatTableDataSource();
+    
   }
+  ngAfterViewInit() {
+    this.paginator.page.subscribe((event: PageEvent) => {
+      this.page.emit(event);
+    });
+  }
+  
   ngOnChanges(changes: SimpleChanges) {
     if (changes['data']) {
       this.dataSource.data = this.data;
-      this.dataSource.paginator = this.paginator
+      
       console.log('Data source updated:', this.dataSource.data); // Verify data is assigned
     }
+    if (changes['totalLength'] && this.paginator) {
+      console.log('Paginator length set to:', this.paginator.length); // Verify paginator length
+    }
+  }
+  getNestedValue(element: any, path: string): any {
+    return path.split('.').reduce((acc, part) => acc && acc[part], element);
   }
 }
