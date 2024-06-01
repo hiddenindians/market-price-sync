a3df5c79bfabb75b027ae2a7181c718403788383
hiddenindians <amanthethy@gmail.com>
2024-06-01 01:05:23 -0500

    Implementing services. building lists
    not a meaningful commit. for greptile.

--- frontend/src/app/routes/dashboard/dashboard.component.ts
+++ frontend/src/app/routes/dashboard/dashboard.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FeathersService } from '../../services/api/feathers.service';
-import { DataService } from '../../services/data/data.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { FeathersService } from '../../services/api/feathers.service'
+import { DataService } from '../../services/data/data.service'
+import { Subscription } from 'rxjs'
+import { DataTableComponent } from '../../shared/data-table/data-table.component'
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
+import { MatSortModule } from '@angular/material/sort'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DataTableComponent, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.less'
 })
 export class DashboardComponent implements OnInit {
+  games: any[] = []
+  displayedColumns: string[]  = ['logo','name', 'external_id.tcgcsv_id']
 
-  constructor(private data: DataService){}
-  ngOnInit(){
-   this.data.getGames().subscribe((data:any) => {
-    console.log(data)
-   })
+
+  constructor(private data: DataService) {}
+  ngOnInit() {
+   this.fetchGames(10,0)
+  }
+
+  fetchGames(limit: number, skip: number) {
+    this.data.getGames(limit, skip).subscribe((data: any) => {
+      this.games = data.data
+      console.log('Fetched games:', this.games); // Verify data is fetched
+    })
+  }
+
+  onPageChange(event: any) {
+    const limit = event.pageSize;
+    const skip = event.pageIndex * event.pageSize;
+    this.fetchGames(limit, skip);
   }
 }
